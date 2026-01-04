534fac45de6f611be712e38ec442b16eb2a0264c
FranciscoJacinto7077 <franciscojacinto007@gmail.com>
2026-01-04 02:32:00 +0000

    Add Spring Boot application entry point
    Annotated GestaoDespesasApplication with @SpringBootApplication and added the main method to enable Spring Boot application startup.

--- backend/src/main/java/upt/gestaodespesas/GestaoDespesasApplication.java
+++ backend/src/main/java/upt/gestaodespesas/GestaoDespesasApplication.java
@@ -1,5 +1,11 @@
 package upt.gestaodespesas;
 
-public class GestaoDespesasApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class GestaoDespesasApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDespesasApplication.class, args);
+	}	
 }
