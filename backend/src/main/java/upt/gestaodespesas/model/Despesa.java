de9553d438e7f2cbe655695eccda272336f63da3
FranciscoJacinto7077 <franciscojacinto007@gmail.com>
2026-01-04 03:12:31 +0000

    Add JPA annotations and fields to Despesa entity
    Introduced JPA annotations (@Entity, @Table) and added fields with getters and setters to the Despesa class for persistence. This prepares the model for integration with a relational database.

--- backend/src/main/java/upt/gestaodespesas/model/Despesa.java
+++ backend/src/main/java/upt/gestaodespesas/model/Despesa.java
@@ -1,5 +1,48 @@
 package upt.gestaodespesas.model;
 
-public class Despesa {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "despesas")
+public class Despesa {
+	
+	private	 Long id;
+	private String descricao;
+	private Double valor;
+	private String data;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Double getValor() {
+		return valor;
+	}
+	
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	public String getData() {
+		return data;
+	}
 }
