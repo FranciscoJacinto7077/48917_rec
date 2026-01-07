305001280e3fc3cc15f5c0862221ab60f0b64915
FranciscoJacinto7077 <franciscojacinto007@gmail.com>
2026-01-07 18:49:35 +0000

    Add Categoria entity model
    Introduces the Categoria class as a JPA entity with id and nome fields, including validation and database mapping annotations.

---
+++ backend/src/main/java/upt/gestaodespesas/model/Categoria.java
@@ -0,0 +1,39 @@
+package upt.gestaodespesas.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column(nullable = false, unique = true)
+	private String nome;
+	
+	public Categoria() {}
+	
+	public Long getId() {
+		return id;
+	
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	
+	}
+	
+	public String getNome() {
+		return nome;
+		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
