f220ae2728956f0ca1686465496271d72f1cfa56
FranciscoJacinto7077 <franciscojacinto007@gmail.com>
2026-01-24 00:03:41 +0000

    Add Utilizador model class
    Introduces the Utilizador class with fields for id, username, gmail, and password, including getters and setters. This class will represent a user entity in the application.

---
+++ backend/src/main/java/upt/gestaodespesas/model/Utilizador.java
@@ -0,0 +1,38 @@
+package upt.gestaodespesas.model;
+
+public class Utilizador {
+	
+	private Long id;
+	private String username;
+	private String gmail;
+	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getGmail() {
+		return gmail;
+	}
+	
+	public void setGmail(String gmail) {
+		this.gmail = gmail;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
+
